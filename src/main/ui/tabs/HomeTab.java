5fa1c3baad5e9eaf7825e2485d05b9d613a184f8
emily hou <xhou04@students.cs.ubc.ca>
2024-11-21 20:50:59 -0800

    HomeTab class specification & implementation

--- src/main/ui/tabs/HomeTab.java
+++ src/main/ui/tabs/HomeTab.java
@@ -9,4 +9,75 @@ import ui.FinancialManagementApp;
 
 public class HomeTab extends Tab {
 
+    private static final String INIT_GREETING = "Welcome To Financial Management System. What would you like to do?";
+    private JLabel greeting;
+
+    // EFFECTS: constructs a home tab for console with buttons and a greeting
+    public HomeTab(FinancialManagementApp controller) {
+        super(controller);
+
+        setLayout(new GridLayout(3, 1));
+
+        placeGreeting();
+        placeBookButtons();
+        placeReportButton();
+    }
+
+    // EFFECTS: creates greeting at top of console
+    private void placeGreeting() {
+        greeting = new JLabel(INIT_GREETING, JLabel.CENTER);
+        greeting.setSize(WIDTH, HEIGHT / 3);
+        this.add(greeting);
+    }
+
+    // EFFECTS: construts Income and ExpenseBook buttons that switches tab on the
+    // console
+    private void placeBookButtons() {
+        JButton b1 = new JButton(ButtonNames.IncomeBook.getValue());
+        JButton b2 = new JButton(ButtonNames.ExpenseBook.getValue());
+
+        JPanel buttonRow = formateButtonRow(b1);
+        buttonRow.add(b2);
+
+        b1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String buttonPressed = e.getActionCommand();
+                if (buttonPressed.equals(ButtonNames.IncomeBook.getValue())) {
+                    getController().getTabbedPane().setSelectedIndex(FinancialManagementApp.INCOMEBOOK_TAB_INDEX);
+                }
+            }
+        });
+
+        b2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String buttonPressed = e.getActionCommand();
+                if (buttonPressed.equals(ButtonNames.ExpenseBook.getValue())) {
+                    getController().getTabbedPane().setSelectedIndex(FinancialManagementApp.EXPENSEOOK_TAB_INDEX);
+                }
+            }
+        });
+
+        this.add(buttonRow);
+    }
+
+    // EFFECTS: construts a button that switches to the report tab on the console
+    private void placeReportButton() {
+        JPanel reportBlock = new JPanel();
+        JButton reportButton = new JButton(ButtonNames.GO_TO_REPORT.getValue());
+        reportBlock.add(formateButtonRow(reportButton));
+
+        reportButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String buttonPressed = e.getActionCommand();
+                if (buttonPressed.equals(ButtonNames.GO_TO_REPORT.getValue())) {
+                    getController().getTabbedPane().setSelectedIndex(FinancialManagementApp.REPORT_TAB_INDEX);
+                }
+            }
+        });
+
+        this.add(reportBlock);
+    }
 }

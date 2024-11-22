01cdf4162c6e48735d4b8ea42286c50f66424cc1
emily hou <xhou04@students.cs.ubc.ca>
2024-11-21 20:46:21 -0800

    SplashScreen class specification & Implementation

--- src/main/ui/SplashScreen.java
+++ src/main/ui/SplashScreen.java
@@ -9,5 +9,38 @@ import java.io.IOException;
 
 // Represents a splash screen that is shown for three seconds once the user open the app
 public class SplashScreen extends JWindow {
+    private BufferedImage myPicture;
 
+    public SplashScreen() {
+        try {
+            myPicture = ImageIO.read(new File("src/main/Image/fms.png"));
+        } catch (IOException e) {
+            System.out.println("Pic not Found");
+        }
+
+        ImageIcon splashImage = new ImageIcon(myPicture);
+        JLabel imageLabel = new JLabel(splashImage);
+        getContentPane().add(imageLabel);
+
+        setSize(splashImage.getIconWidth(), splashImage.getIconHeight());
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        setLocation(
+                (screenSize.width - getWidth()) / 2,
+                (screenSize.height - getHeight()) / 2);
+    }
+
+    // EFFECTS: make the splash screen visible to user once they openned the app
+    public void showSplash(int time) {
+        setVisible(true);
+
+        Timer timer = new Timer(time, e -> {
+            setVisible(false);
+            dispose();
+        });
+
+        timer.setRepeats(false);
+        timer.start();
+    }
 }
+
+

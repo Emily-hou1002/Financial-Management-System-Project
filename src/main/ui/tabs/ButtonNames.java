0c49be06ca62718f624b6a3f938098b50f7d4b61
emily hou <xhou04@students.cs.ubc.ca>
2024-11-21 20:49:32 -0800

    ButtonNames enum class implementation

--- src/main/ui/tabs/ButtonNames.java
+++ src/main/ui/tabs/ButtonNames.java
@@ -1,5 +1,27 @@
 package ui.tabs;
 
 public enum ButtonNames {
+    IncomeBook("Go to IncomeBook"),
+    ExpenseBook("Go to ExpenseBook"),
+    GO_TO_REPORT("Current Report"),
+    ADD("Add new record"),
+    Remove("Remove record"),
+    SAVE("Save record"),
+    LOAD("Load record"),
+    DISPLAY("Display book"),
+    ANNUAL("Annual Balance"),
+    MONTHLY("Monthly Balance");
+
+    private final String name;
+
+    ButtonNames(String name) {
+        this.name = name;
+    }
+
+    // EFFECTS: returns name value of this button
+    public String getValue() {
+        return name;
+    }
 
 }
+

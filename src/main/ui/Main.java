0450da813a25a2aacaeee9b4e7957c358fed25c1
emily hou <xhou04@students.cs.ubc.ca>
2024-11-21 20:47:01 -0800

    SplashScreen Initialization in Main

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -4,6 +4,14 @@ import java.io.FileNotFoundException;
 
 public class Main {
     public static void main(String[] args) {
+        SplashScreen splash = new SplashScreen();
+        splash.showSplash(4000);
+        
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            System.out.println("Splash screen interrupted");
+        }
         try {
             new FinancialManagementApp();
         } catch (FileNotFoundException e) {
@@ -13,3 +21,4 @@ public class Main {
 
     }
 }
+

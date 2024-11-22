12c16669b957780de6088695da77c704143875e5
emily hou <xhou04@students.cs.ubc.ca>
2024-11-21 20:48:13 -0800

    abstract Tab class specification & Implementation

--- src/main/ui/tabs/Tab.java
+++ src/main/ui/tabs/Tab.java
@@ -11,4 +11,27 @@ import ui.FinancialManagementApp;
 
 public abstract class Tab extends JPanel {
 
+    private final FinancialManagementApp controller;
+    protected IncomeBook icbk;
+    protected ExpenseBook epbk;
+
+    // REQUIRES: FinancialManagementApp controller holds this tab
+    public Tab(FinancialManagementApp controller) {
+        this.controller = controller;
+    }
+
+    // EFFECTS: creates and returns row with button included
+    public JPanel formateButtonRow(JButton b) {
+        JPanel p = new JPanel();
+        p.setLayout(new FlowLayout());
+        p.add(b);
+
+        return p;
+    }
+
+    // EFFECTS: returns the FinancialManagementApp controller for this tab
+    public FinancialManagementApp getController() {
+        return controller;
+    }
+
 }
